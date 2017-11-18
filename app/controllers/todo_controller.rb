c9c33b060a4576b68f3b3a439431e63dc44ab2ce
Karina Montiel <karinamontiel375@gmail.com>
2017-11-18 22:17:31 +0000

    Adding If and Elsif in the Show Controller

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,7 +2,14 @@ class TodoController <ApplicationController
         def index
         end
         def show
-            @todo_description="Make a Curriculum"
-            @todo_pomodoro_estimate= 4
-        end
+            todo_id= params[:id]
+            if todo_id=='1'
+                @todo_description="Make a Curriculum"
+                @todo_pomodoro_estimate= 4
+                
+            elsif todo_id=='2'
+                @todo_description= "Buy Workshop Supplies"
+                @todo_pomodoro_estimate= 3
+            end
+        end 
 end
