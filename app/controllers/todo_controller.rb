a3f3833566867b15e152b4102ef9b244ddadf657
Karina Montiel <karinamontiel375@gmail.com>
2017-11-19 19:41:22 +0000

    Updated Code

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,34 +2,6 @@ class TodoController <ApplicationController
     def index
     end
     def show
-        todo_id= params[:id]
-        if todo_id=='1'
-            @todo_description="Make a Curriculum"
-            @todo_pomodoro_estimate= 4
-            
-        elsif todo_id=='2'
-            @todo_description= "Buy Workshop Supplies"
-            @todo_pomodoro_estimate= 3
-
-        elsif todo_id=='3'
-            @todo_description= "Meet with the Volunteer Trainers"
-            @todo_pomodoro_estimate= 2
-            
-        elsif todo_id=='4'
-            @todo_description= "Order Food for Saturday and Sunday"
-            @todo_pomodoro_estimate= 1
-        
-        elsif todo_id=='5'
-            @todo_description= "Check Pre-Work Assignments"
-            @todo_pomodoro_estimate= 2
-
-        elsif todo_id=='6'
-            @todo_description= "Send Workshop Location to all the students"
-            @todo_pomodoro_estimate= 1
-            
-        elsif todo_id=='7'
-            @todo_description= "Have A Great Workshop"
-            @todo_pomodoro_estimate= 0
-        end
+        @todo = Todo.find_by_id(params[:id])
     end
 end
